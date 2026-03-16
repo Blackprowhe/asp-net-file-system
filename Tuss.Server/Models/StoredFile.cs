0ac8bc2882970a646553abc1b162d0c734b58208
disco <discovicke@gmail.com>
2026-03-16 12:00:40 +0100

    add StoredFile model to represent file data attributes

---
+++ Tuss.Server/Models/StoredFile.cs
@@ -0,0 +1,13 @@
+﻿namespace Tuss.Server.Models;
+
+public class StoredFile
+{
+    public string Name { get; set; } = "";
+    public string Content { get; set; } = "";
+    public string Created { get; set; } = "";
+    public string Changed { get; set; } = "";
+    public bool IsFile { get; set; } = true;
+    public long Bytes { get; set; }
+    public string Extension { get; set; } = "";
+}
+
