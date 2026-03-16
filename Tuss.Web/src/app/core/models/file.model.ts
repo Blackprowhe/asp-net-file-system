49ae78dc81076e42a326f0e82ef5e266d1eeef90
disco <discovicke@gmail.com>
2026-03-16 15:40:21 +0100

    add file model interfaces for stored files and versioning

---
+++ Tuss.Web/src/app/core/models/file.model.ts
@@ -0,0 +1,19 @@
+﻿export interface StoredFile {
+  name: string;
+  created: string;
+  changed: string;
+  file: boolean;
+  bytes: number;
+  extension: string;
+  currentVersion: number;
+}
+
+export interface FileVersion {
+  version: number;
+  createdAt: string;
+  bytes: number;
+  isCurrent: boolean;
+}
+
+export type FileMap = Record<string, StoredFile>;
+
