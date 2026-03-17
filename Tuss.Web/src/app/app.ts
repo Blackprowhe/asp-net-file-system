551f97c099c20f18dbe5ef37a9138fb3021e6dc6
Viktor Johansson <discovicke@gmail.com>
2026-03-17 15:47:19 +0100

    feat: remove file upload and folder creation components from main app

--- Tuss.Web/src/app/app.ts
+++ Tuss.Web/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { FileBrowserComponent } from './features/file-browser/file-browser.component';
-import { FileUploadComponent } from './features/file-upload/file-upload.component';
-import { FolderCreateComponent } from './features/folder-create/folder-create.component';
 import { SidebarComponent } from './features/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FileBrowserComponent, FileUploadComponent, FolderCreateComponent, SidebarComponent],
+  imports: [FileBrowserComponent, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
