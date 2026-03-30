db4e4738ec0544f21b950cf427a5e92a46751fd3
Blackprowhe <ljungqvistchala98@gmail.com>
2026-03-30 23:12:25 +0200

    skpad url hantering i clients

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,17 +1,48 @@
-﻿public class Program
+﻿using System.Net.Http;
+
+public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
+        // 1. Saknas URL
         if (args.Length < 2)
         {
             Console.WriteLine("ingen url hittades");
             Environment.Exit(1);
         }
-    }
-    
-}
-    
-        
 
-    
-    
+        var baseUrl = args[1];
+
+        // 2. Lägg till rätt protokoll
+        if (!baseUrl.StartsWith("http://") && !baseUrl.StartsWith("https://"))
+        {
+            if (baseUrl.Contains("localhost"))
+            {
+                baseUrl = "http://" + baseUrl;
+            }
+            else
+            {
+                baseUrl = "https://" + baseUrl;
+            }
+        }
+
+        var url = baseUrl + "/api/files";
+
+        try
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync(url);
+
+            // 3. Om servern svarar men inte är okej
+            if (!response.IsSuccessStatusCode)
+            {
+                Environment.Exit(1);
+            }
+        }
+        catch
+        {
+            // 4. Om request kraschar (ogiltig URL / ingen server)
+            Environment.Exit(1);
+        }
+    }
+}
