9cb41928dfb8d8d0b874dd00f52fb86abd9f37d3
Blackprowhe <ljungqvistchala98@gmail.com>
2026-03-30 22:48:21 +0200

    skapat class och påbörjad argument hantering

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length < 2)
+        {
+            Console.WriteLine("ingen url hittades");
+            Environment.Exit(1);
+        }
+    }
+    
+}
+    
+        
+
+    
+    
